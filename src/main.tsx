bf636fb80371861354befe7f9f5c8756b4c17738
JonathasLopes <jon.athas@hotmail.com>
2025-04-23 14:47:04 -0300

    mobile css added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/styles/colors.css';
 import './assets/styles/global.css';
+import './assets/styles/@media.css';
 import App from './App.tsx';
 import { ToastContainer } from 'react-toastify';
 
